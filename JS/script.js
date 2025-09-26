3992fb359597869b25ef1eb079b9ff63dd61d1a5
creative-idba <webcreativeidba6@gmail.com>
2025-09-26 15:54:39 +0700

    two commit

--- JS/script.js
+++ JS/script.js
@@ -9,7 +9,6 @@ function updateDisplay() {
   document.getElementById("display").textContent = `${hrs}:${mins}:${secs}`;
 }
 
-// Tombol Set waktu
 document.getElementById("set").addEventListener("click", function () {
   let h = parseInt(document.getElementById("hours").value) || 0;
   let m = parseInt(document.getElementById("minutes").value) || 0;
@@ -18,7 +17,6 @@ document.getElementById("set").addEventListener("click", function () {
   updateDisplay();
 });
 
-// Start timer
 document.getElementById("start").addEventListener("click", function () {
   if (!running && totalSeconds > 0) {
     running = true;
@@ -35,16 +33,54 @@ document.getElementById("start").addEventListener("click", function () {
   }
 });
 
-// Pause timer
 document.getElementById("pause").addEventListener("click", function () {
   clearInterval(timer);
   running = false;
 });
 
-// Reset timer
 document.getElementById("reset").addEventListener("click", function () {
   clearInterval(timer);
   running = false;
   totalSeconds = 0;
   updateDisplay();
 });
+
+document.querySelectorAll(".content_tim1 input, .content_tim2 input").forEach(input => {
+  input.addEventListener("input", function() {
+    this.style.width = "auto";
+    let newWidth = this.scrollWidth + 10;
+    if (newWidth < 100) newWidth = 100;
+    if (newWidth > 400) newWidth = 400;
+    this.style.width = newWidth + "px";
+  });
+});
+ 
+function openFullscreen() {
+  let elem = document.documentElement;
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { 
+    elem.msRequestFullscreen();
+  }
+}
+
+const contentInputs = document.querySelector(".content_inputs");
+document.addEventListener("fullscreenchange", () => {
+  if (document.fullscreenElement) {
+    contentInputs.style.display = "none";
+  } else {
+    contentInputs.style.display = "block";
+  }
+});
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
